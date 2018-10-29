5019b680480c154f8be165a3026e44db4375dec1
Shaurya <35784097+shauryg@users.noreply.github.com>
2018-10-29 18:33:17 -0400

    Update proj5.c

--- Project 5/proj5.c
+++ Project 5/proj5.c
@@ -8,6 +8,7 @@
 #define CONTOUR_ROWS 7
 #define SQR(x) ((x)*(x))
 #define WINDOW 7
+#define TEST_VALUE 0
 
 void read_initial_contours(char [], int** , int**, int *);
 void find_max_min(float [],  int, int *, int *);
@@ -17,11 +18,11 @@ void sobel_filter(unsigned char * , unsigned char * , int , int );
 int main(int argc, char **argv){
 	FILE	*fpt;
 	int		ROWS1,COLS1,BYTES1;
-    char	header[320];
-    unsigned char	*image, *initial_contour, *sobel_out, *final_contour;
+  char	header[320];
+  unsigned char	*image, *initial_contour, *final_contour, *sobel_out;
 	int *crow, *ccol, *next_row, *next_col;
 	int i, j, r, c, size, max, min;
-	float energy[49], temp[49];
+	float energy[WINDOW*WINDOW], temp[WINDOW*WINDOW];
 	float avg_distance = 0, min_c;
 	int total;
 
@@ -67,7 +68,6 @@ int main(int argc, char **argv){
 	
 /*-------------------CALCULATE AVG DISTANCE------------------------------*/
 
-	
 	next_row = (int *)calloc(size, sizeof(int));
 	next_col = (int *)calloc(size, sizeof(int));
 	
@@ -76,73 +76,92 @@ int main(int argc, char **argv){
 	sobel_out = (unsigned char *)calloc(ROWS1*COLS1,sizeof(unsigned char));	
 	sobel_filter(image, sobel_out, ROWS1, COLS1);
 
+  
 /*--------------------CONTOURING---------------------*/
 
 	int k = 0;
 	i = 0;
+  
 	for(total = 0; total < 30; total++){
-		for(i = 0; i < size; i++){
+		for(i = 0; i < 42; i++){
+   
 		/*--------------------CALCULATE FIRST INTERNAL ENERGY---------------------*/
-
 			for(r = -3; r <= 3; r++){
 				for(c = -3; c <= 3; c++){
-					energy[(r + 3)*WINDOW + (c + 3)] = SQR((crow[i] + r) - crow[(i + 1)%42]) + SQR((ccol[i] + c) - ccol[(i + 1)%42]);
+					energy[(r + 3)*WINDOW + (c + 3)] = SQR((crow[i] + r) - crow[(i + 1)%size]) + SQR((ccol[i] + c) - ccol[(i + 1)%size]);
 				}
 			}
 
-			find_max_min(energy, 49, &max, &min);
-			normalize(energy, max, min, 49, 1, 0);
-	
-		/*--------------------CALCULATE SECOND INTERNAL ENERGY---------------------*/
+		/*--------------------CALCULATE SECOND INTERNAL ENERGY---------------------*/ 
+      
       avg_distance = 0.0;
 			for(k = 0; k < size; k++){
-				avg_distance += sqrt(SQR(crow[k] - crow[(k+1)%42]) + SQR(ccol[k] - ccol[(k+1)%42]));		
+				avg_distance = avg_distance + sqrt(SQR(crow[k] - crow[(k+1)%size]) + SQR(ccol[k] - ccol[(k+1)%size]));
 			}
-		  avg_distance /= size;  
+		  avg_distance /= (size); 
       
 			for(r = -3; r <= 3; r++){
 				for(c = -3; c <= 3; c++){
-          temp[(r + 3)*WINDOW + (c + 3)] = SQR(avg_distance - sqrt(SQR(crow[i] + r - crow[(i + 1)%42]) + SQR(ccol[i] + r - ccol[(i + 1)%42])));
-				}
+				  temp[(r + 3)*WINDOW + (c + 3)] = SQR(avg_distance - sqrt(energy[(r + 3)*WINDOW + (c + 3)]));
+        }
 			}
+      
+      find_max_min(energy, WINDOW*WINDOW, &max, &min);
+			normalize(energy, max, min, WINDOW*WINDOW, 1, 0);
 	
-			find_max_min(temp, 49, &max, &min);
-			normalize(temp, max, min, 49, 1, 0);
-
-			for(k = 0; k < 49; k++){
+			find_max_min(temp, WINDOW*WINDOW, &max, &min);
+			normalize(temp, max, min, WINDOW*WINDOW, 1, 0);
+      
+			for(k = 0; k < WINDOW*WINDOW; k++){
 				energy[k] += temp[k];
 			}
 
 		/*--------------------CALCULATE EXTERNAL ENERGY---------------------*/
-			for(r = -3; r <= 3; r++){
+      k = 0;
+      for(r = -3; r <= 3; r++){
 				for(c = -3; c <= 3; c++){
 					temp[(r + 3)*WINDOW + (c + 3)] = SQR(sobel_out[(crow[i] + r)*COLS1 + (ccol[i] + c)]);
+          k++;
 				}
 			}
 	
-			find_max_min(temp, 49, &max, &min);
-			normalize(temp, max, min, 49, 1, 0);
+			find_max_min(temp, WINDOW*WINDOW, &max, &min);
+			normalize(temp, max, min, WINDOW*WINDOW, 1, 0);
 
-			for(k = 0; k < 49; k++){
+			for(k = 0; k < WINDOW*WINDOW; k++){
 				energy[k] += temp[k];
-			}
+        if(i == TEST_VALUE){
+          if(k%7 == 0)
+            printf("\n");
+          printf(" %f ", energy[k]);
+          if(k == 48)
+            printf("\n");
+        }
+			}      
+      
 		/*--------------------CALCULATE CHANGED CONTOUR POINT---------------------*/
 			min_c = energy[0];
 			for(k = 0; k < WINDOW*WINDOW; k++){
-				if(min_c > energy[k]){
+				if(min_c >= energy[k]){
 					min_c = energy[k];
         }	
 			}
+      
+      //if(i == TEST_VALUE);
+        //printf("\n%f\n", min_c);
 
 			for(r = -3; r <= 3; r++){
 				for(c = -3; c <= 3; c++){
-					if(energy[(r + 3)*WINDOW + (c+3)] == min_c){
+					if(energy[(r + 3)*WINDOW + (c + 3)] == min_c){
 						next_row[i] = crow[i] + r;
-						next_col[i] = ccol[i] + c;				
+						next_col[i] = ccol[i] + c;	
+            if(i == TEST_VALUE)
+            printf("\n%d %d %f\n", r, c, min_c);		
 					}
 				}
 			}
-		}
+		} // END OF LOOPING THROUGH CONTOUR POINTS
+
 	/*--------------------CHANGE THE CONTOUR POINTS---------------------*/
 		for(k = 0; k < size; k++){
 			crow[k] = next_row[k];
@@ -167,6 +186,12 @@ int main(int argc, char **argv){
 		}
 	}
 	
+  fpt=fopen("final_contours.txt","w");
+  for(i = 0; i < 42; i++){
+	  fprintf(fpt,"%d %d\n",ccol[i],crow[i]);
+  }
+	fclose(fpt);
+ 
 	fpt=fopen("final_contours.ppm","w");
 	fprintf(fpt,"P5 %d %d 255\n",COLS1,ROWS1);
 	fwrite(final_contour,COLS1*ROWS1,1,fpt);
@@ -177,49 +202,44 @@ int main(int argc, char **argv){
 
 void sobel_filter(unsigned char * image, unsigned char * output, int ROWS, int COLS){
 
-	int sobel_x[9] = {1,0,-1,2,0,-2,1,0,-1};
-	int sobel_y[9] = {1,2,1,0,0,0,-1,-2,-1};
-	int i, r, c, sobel_r, sobel_c, sum = 0, max, min;
-	float *temp, *temp2;
-	float *temp_output;
+	int sobel_x[9] = {1,0,-1,2,0,-3,1,0,-1};
+	int sobel_y[9] = {1,2,1,0,0,0,-1,2,-1};
+	int i, r, c, sobel_r, sobel_c, sumx = 0, sumy = 0, max_out, min_out;
+	float *temp, *post_normalized;
 	FILE *fpt;
 
-	temp =(float *)calloc(ROWS*COLS,sizeof(float));	
-	temp2 =(float *)calloc(ROWS*COLS,sizeof(float));	
-	temp_output = (float *)calloc(ROWS*COLS, sizeof(float));
-
+	temp =(float *)calloc(ROWS*COLS,sizeof(float));		
+  post_normalized = (float *)calloc(ROWS*COLS,sizeof(float));		
+  
 	for(r = 1; r < ROWS - 1; r++){
 		for(c = 1; c < COLS - 1; c++){
-			sum = 0;
+			sumx = sumy = 0;
 			for(sobel_r = -1; sobel_r <= 1; sobel_r++){
 				for(sobel_c = -1; sobel_c <= 1; sobel_c++){
-					sum += (sobel_x[(sobel_r + 1)*3 + (sobel_c + 1)] * image[(r + sobel_r)*COLS + c + sobel_c]);
+					sumx += (sobel_x[(sobel_r + 1)*3 + (sobel_c + 1)] * image[(r + sobel_r)*COLS + (sobel_c + c)]);
+          sumy += (sobel_y[(sobel_r + 1)*3 + (sobel_c + 1)] * image[(r + sobel_r)*COLS + (sobel_c + c)]);
 				}		
 			}
-			temp[r*COLS + c] = sum*sum;		
-		}	
-	}
-
-	for(r = 1; r < ROWS - 1; r++){
-		for(c = 1; c < COLS - 1; c++){
-			sum = 0;
-			for(sobel_r = -1; sobel_r <= 1; sobel_r++){
-				for(sobel_c = -1; sobel_c <= 1; sobel_c++){
-					sum += (sobel_y[(sobel_r + 1)*3 + (sobel_c + 1)] * image[(r + sobel_r)*COLS + c + sobel_c]);
-				}		
-			}
-			temp2[r*COLS + c] = sum*sum;		
+			temp[r*COLS + c] = (sumx*sumx) + (sumy*sumy);		
 		}	
 	}
 	
-	for(i = 0; i < ROWS*COLS; i++)
-		temp_output[i] = sqrt(temp[i] + temp2[i]);
-		
-	find_max_min(temp_output, ROWS*COLS, &max, &min);
-	normalize(temp_output, max, min, ROWS*COLS, 255, 0);
-	
-	for(i = 0; i < ROWS*COLS; i++)
-		output[i] = 255 - (unsigned char)temp_output[i];
+	for(i = 0; i < ROWS*COLS; i++){
+    temp[i] = sqrt(temp[i]);
+    post_normalized[i] = temp[i];
+    //printf(" %f ", temp[i]);
+  }
+  
+  find_max_min(temp, ROWS*COLS, &max_out, &min_out);
+	normalize(temp, max_out, min_out, ROWS*COLS, 255, 0);
+  
+	//find_max_min(temp, ROWS*COLS, &max_out, &min_out);
+	//normalize(temp, max_out, min_out, ROWS*COLS, 1, 0);
+
+	for(i = 0; i < ROWS*COLS; i++){
+		output[i] = 255 - (unsigned char)temp[i];
+    //output[i] = temp[i];
+  }
 	
 	fpt=fopen("sobel_out.ppm","w");
 	fprintf(fpt,"P5 %d %d 255\n",COLS,ROWS);
@@ -232,7 +252,7 @@ void find_max_min(float energy[], int size, int *max, int *min){
 	
 	int i;
 	*max = *min = energy[0];
-	
+ 
 	for(i = 0; i < size; i++){
 		if(energy[i] > *max)
 			*max = energy[i];
