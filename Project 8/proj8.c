3ce85dcab57d7f1aad0e05bf319f7fb51e9541d6
Shaurya <35784097+shauryg@users.noreply.github.com>
2018-11-30 18:50:33 -0500

    Update proj8.c

--- Project 8/proj8.c
+++ Project 8/proj8.c
@@ -8,18 +8,22 @@
 #define COLS 128
 #define DISTANCE 3 //Pixels
 
+#define SQR(x) ((x)*(x))
+
 void get_3D_points(double **);
 
-void calc_surface_norm(double **, double **, int, int, distance);
+void calc_surface_norm(double **, double **, int, int, int);
+
+
 
 int main(void){
 
-	FILE *fpt;
-	int	 ROWS1,COLS1,BYTES1;
+  FILE *fpt;
+  int	 ROWS1,COLS1,BYTES1;
   char header[320];
-	unsigned char *image, *binary;
-	double **P;
-  double **S;
+  unsigned char *image, *binary, *threshold;
+  double **P, **S, *D;
+
 
 	int i;
 	/*--------------------OPEN AND READ INPUT IMAGE---------------------*/
@@ -34,12 +38,12 @@ int main(void){
 		  exit(0);
 	}
 
-    image = (unsigned char *)calloc(ROWS1*COLS1,sizeof(unsigned char));
-    header[0]=fgetc(fpt);	/* read white-space character that separates header */
-	  fread(image,1,COLS1*ROWS1,fpt);
-	  fclose(fpt);
+  image = (unsigned char *)calloc(ROWS1*COLS1,sizeof(unsigned char));
+  header[0]=fgetc(fpt);	/* read white-space character that separates header */
+  fread(image,1,COLS1*ROWS1,fpt);
+  fclose(fpt);
 
-	/*--------------------THRESHOLD THE RANGE IMAGE---------------------*/
+	/*--------------------THRESHOLD BASED ON PIXEL---------------------*/
 
 	binary = (unsigned char *)calloc(ROWS1*COLS1,sizeof(unsigned char));
 
@@ -58,7 +62,7 @@ int main(void){
 	get_3D_points(P);
 
 	for(i = 0; i < 1; i++){
-		printf("\n%lf %lf %lf\n", P[0][i], P[1][i], P[2][i]);
+		printf("\n3d coordinates pixel %d : %lf %lf %lf\n", i, P[0][i], P[1][i], P[2][i]);
 	}
 
 	/*----------------------CALCULATE SURFACE NORMALS---------------------*/
@@ -67,88 +71,125 @@ int main(void){
   for(i = 0; i < 3; i++)
     *(S + i) = (double *)calloc(ROWS1 * COLS1, sizeof(double));
 
-  
+  calc_surface_norm(S, P, ROWS1, COLS1, DISTANCE);
 
+  for(i = 0; i < 1; i++){
+		printf("\nSurface Normals pixel %d : %lf %lf %lf\n", i, S[0][i], S[1][i], S[2][i]);
+	}
+ 
+  /*-----------------------CALCULATE DISTANCE -------------------------*/
+  
+  D = (double *)calloc(ROWS1*COLS1, sizeof(double));
+  
+  for(i = 0; i < ROWS1 * COLS1; i++){
+    D[i] = sqrt(SQR(S[0][i]) + SQR(S[1][i]) + SQR(S[2][i]));   
+  }
+  
+  for(i = 0; i < 1; i++){
+		printf("\nDistances %d : %lf\n", i, D[i]);
+	}
+ 
+  /*---------------------THRESHOLD BASED ON DISTANCE------------------*/
+  
+  threshold = (unsigned char *)calloc(ROWS1*COLS1,sizeof(unsigned char));
+
+	for(i = 0; i < (ROWS1*COLS1); i++){
+		if(D[i] < 50)
+        	threshold[i] = 255;
+  }
+  
+ 	fpt=fopen("chair-threshold.ppm","w");
+	fprintf(fpt,"P5 %d %d 255\n",COLS1,ROWS1);
+	fwrite(threshold,COLS1*ROWS1,1,fpt);
+	fclose(fpt); 
+ 
 	return 0;
 }
 
-
 void calc_surface_norm(double **S, double **P, int row, int col, int distance){
 
-  int r, c;
+  int r, c, i;
+  double A[3], B[3];
+  int x = 0, y = 1, z = 2;
   
-  for(r = 0; r = row - distance; r++){
-    for(c = 0; c = c - distance; c++){
-      
+  for(r = 0; r < row - distance; r++){
+    for(c = 0; c < col - distance; c++){
+      for(i = 0; i < 3; i++){
+        A[i] = P[i][(r + distance)*col + (c)] - P[i][(r)*col + (c)];
+        B[i] = P[i][(r)*col + (c + distance)] - P[i][(r)*col + (c)];
+      }
+      S[0][r*col + c] = A[y]*B[z] - A[z]*B[y];
+      S[1][r*col + c] = A[z]*B[x] - A[x]*B[z];
+      S[2][r*col + c] = A[x]*B[y] - A[y]*B[x];
     }
   } 
 } 
 
 void get_3D_points(double **P){
 
-	int	r,c;
-	double cp[7];
-	double xangle,yangle,dist;
-	double ScanDirectionFlag,SlantCorrection;
-	unsigned char RangeImage[ROWS*COLS];
-	int ImageTypeFlag;
-	char Filename[160];
-	FILE *fpt;
-
-	strcpy(Filename, "chair-range.ppm");
-
-	if ((fpt=fopen(Filename,"r")) == NULL){
-	  printf("Couldn't open %s\n",Filename);
-	  exit(0);
-	}
-	fread(RangeImage,1,128*128,fpt);
-	fclose(fpt);
-
-	ImageTypeFlag = 1;
-
-	cp[0]=1220.7;		/* horizontal mirror angular velocity in rpm */
-	cp[1]=32.0;		/* scan time per single pixel in microseconds */
-	cp[2]=(COLS/2)-0.5;		/* middle value of columns */
-	cp[3]=1220.7/192.0;	/* vertical mirror angular velocity in rpm */
-	cp[4]=6.14;		/* scan time (with retrace) per line in milliseconds */
-	cp[5]=(ROWS/2)-0.5;		/* middle value of rows */
-	cp[6]=10.0;		/* standoff distance in range units (3.66cm per r.u.) */
-
-	cp[0]=cp[0]*3.1415927/30.0;	/* convert rpm to rad/sec */
-	cp[3]=cp[3]*3.1415927/30.0;	/* convert rpm to rad/sec */
-	cp[0]=2.0*cp[0];		/* beam ang. vel. is twice mirror ang. vel. */
-	cp[3]=2.0*cp[3];		/* beam ang. vel. is twice mirror ang. vel. */
-	cp[1]/=1000000.0;		/* units are microseconds : 10^-6 */
-	cp[4]/=1000.0;			/* units are milliseconds : 10^-3 */
-
-	switch(ImageTypeFlag)
-	  {
-	  case 1:		/* Odetics image -- scan direction upward */
-	    ScanDirectionFlag=-1;
-	    break;
-	  case 0:		/* Odetics image -- scan direction downward */
-	    ScanDirectionFlag=1;
-	    break;
-	  default:		/* in case we want to do this on synthetic model */
-	    ScanDirectionFlag=0;
-	    break;
-	  }
+  int	r,c;
+  double cp[7];
+  double xangle,yangle,dist;
+  double ScanDirectionFlag,SlantCorrection;
+  unsigned char RangeImage[ROWS*COLS];
+  int ImageTypeFlag;
+  char Filename[160];
+  FILE *fpt;
   
-	if (ImageTypeFlag != 3){
-		for (r=0; r<ROWS; r++){
-	    	for (c=0; c<COLS; c++){
-				SlantCorrection = cp[3]*cp[1]*((double)c-cp[2]);
-				xangle = cp[0]*cp[1]*((double)c-cp[2]);
-				yangle = (cp[3]*cp[4]*(cp[5]-(double)r)) + SlantCorrection*ScanDirectionFlag;	
-				dist=(double)RangeImage[r*COLS+c]+cp[6];
+  strcpy(Filename, "chair-range.ppm");
+  
+  if ((fpt=fopen(Filename,"r")) == NULL){
+    printf("Couldn't open %s\n",Filename);
+    exit(0);
+  }
+  fread(RangeImage,1,128*128,fpt);
+  fclose(fpt);
+  
+  ImageTypeFlag = 1;
+  
+  cp[0]=1220.7;		/* horizontal mirror angular velocity in rpm */
+  cp[1]=32.0;		/* scan time per single pixel in microseconds */
+  cp[2]=(COLS/2)-0.5;		/* middle value of columns */
+  cp[3]=1220.7/192.0;	/* vertical mirror angular velocity in rpm */
+  cp[4]=6.14;		/* scan time (with retrace) per line in milliseconds */
+  cp[5]=(ROWS/2)-0.5;		/* middle value of rows */
+  cp[6]=10.0;		/* standoff distance in range units (3.66cm per r.u.) */
+  
+  cp[0]=cp[0]*3.1415927/30.0;	/* convert rpm to rad/sec */
+  cp[3]=cp[3]*3.1415927/30.0;	/* convert rpm to rad/sec */
+  cp[0]=2.0*cp[0];		/* beam ang. vel. is twice mirror ang. vel. */
+  cp[3]=2.0*cp[3];		/* beam ang. vel. is twice mirror ang. vel. */
+  cp[1]/=1000000.0;		/* units are microseconds : 10^-6 */
+  cp[4]/=1000.0;			/* units are milliseconds : 10^-3 */
+  
+  switch(ImageTypeFlag)
+    {
+    case 1:		/* Odetics image -- scan direction upward */
+      ScanDirectionFlag=-1;
+      break;
+    case 0:		/* Odetics image -- scan direction downward */
+      ScanDirectionFlag=1;
+      break;
+    default:		/* in case we want to do this on synthetic model */
+      ScanDirectionFlag=0;
+      break;
+    }
+  
+  if (ImageTypeFlag != 3){
+  	for (r=0; r<ROWS; r++){
+      	for (c=0; c<COLS; c++){
+  			SlantCorrection = cp[3]*cp[1]*((double)c-cp[2]);
+  			xangle = cp[0]*cp[1]*((double)c-cp[2]);
+  			yangle = (cp[3]*cp[4]*(cp[5]-(double)r)) + SlantCorrection*ScanDirectionFlag;	
+  			dist=(double)RangeImage[r*COLS+c]+cp[6];
         
         P[2][r*COLS + c] = sqrt((dist*dist)/(1.0+(tan(xangle)*tan(xangle)) + (tan(yangle)*tan(yangle))));
-				P[0][r*COLS + c] = tan(xangle)*P[2][r*COLS + c];
-				P[1][r*COLS + c] = tan(yangle)*P[2][r*COLS + c];
+  			P[0][r*COLS + c] = tan(xangle)*P[2][r*COLS + c];
+  			P[1][r*COLS + c] = tan(yangle)*P[2][r*COLS + c];
         
-	    	}
-	  	}
-	}
+      	}
+    	}
+  }
 }
 
 
